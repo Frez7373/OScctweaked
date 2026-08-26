800191740f5e49521422fcc79b7cd0e02a221171
Frez7373 <danilla.mikhailov@gmail.com>
2026-08-26 03:16:11 +0300

    Add OScctweaked application registry

---
+++ core/apps.lua
@@ -0,0 +1,14 @@
+return {
+ {id="explorer",title="File Explorer",icon="EX",file="programs/explorer.lua",w=44,h=19},
+ {id="calculator",title="Calculator",icon="CAL",file="programs/calculator.lua",w=36,h=19},
+ {id="notes",title="Notepad",icon="TXT",file="programs/notes.lua",w=52,h=20},
+ {id="paint",title="Paint",icon="ART",file="programs/paint.lua",w=52,h=21},
+ {id="clock",title="Clock",icon="CLK",file="programs/clock.lua",w=36,h=15},
+ {id="calendar",title="Calendar",icon="CAL",file="programs/calendar.lua",w=42,h=18},
+ {id="settings",title="Settings",icon="SET",file="programs/settings.lua",w=48,h=19},
+ {id="system",title="Task Manager",icon="SYS",file="programs/system.lua",w=48,h=19},
+ {id="browser",title="Web",icon="WEB",file="programs/browser.lua",w=52,h=20},
+ {id="mail",title="Mail",icon="MAIL",file="programs/mail.lua",w=52,h=20},
+ {id="store",title="App Center",icon="APP",file="programs/store.lua",w=48,h=19},
+ {id="terminal",title="Command Center",icon=">_",file="programs/terminal.lua",w=48,h=19}
+}
